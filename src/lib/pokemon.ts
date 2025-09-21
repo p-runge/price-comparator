a15f28e068cf3fedb4ed44f1d2335a59750f99f0
Philipp Runge <p.runge@progani.com>
2025-09-21 19:56:23 +0200

    fix reading of cards json

--- src/lib/pokemon.ts
+++ src/lib/pokemon.ts
@@ -7,12 +7,10 @@ export function getCardDataFromShort(short: string) {
   return POKEMON_MAP[short.toLowerCase()] ?? null;
 }
 
-export const POKEMON_MAP: Record<string, { set: string; name: string }> = {
-  // ng9: { set: "Neo Genesis", name: "Lugia" },
-};
-
-const res = await fetch("/cards.json");
-const cards = (await res.json()) as Array<{
+// A map containing short, set and name of all pokemon cards
+export const POKEMON_MAP: Record<string, { set: string; name: string }> = {};
+const res = typeof window !== "undefined" && (await fetch("/cards.json"));
+const cards = (res ? await res.json() : []) as Array<{
   short: string;
   setName: string;
   cardName: string;
